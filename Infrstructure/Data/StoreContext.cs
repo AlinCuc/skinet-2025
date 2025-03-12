f47a980f6a27849f5aa83d9b575bb25c2f335ee9
AlinCuc <alin.cucc@gmail.com>
2025-03-12 11:11:24 +0200

    initial commit

---
+++ Infrstructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Infrstructure.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrstructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        }
+    }
+}
