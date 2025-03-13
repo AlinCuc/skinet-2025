685dd96a40407f35e5765e87be2c0b69874844ea
AlinCuc <alin.cucc@gmail.com>
2025-03-13 12:25:13 +0200

    added API arhitecture(ReposiroryPattern)

---
+++ Infrstructure/Data/StoreContextSeed.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrstructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context)
+        {
+            if (!context.Products.Any())
+            {
+                var productData = await File.ReadAllTextAsync("../Infrstructure/Data/SeedData/products.json");
+
+                var products = JsonSerializer.Deserialize<List<Product>>(productData);
+                if (products.IsNullOrEmpty()) return;
+                context.Products.AddRange(products);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
