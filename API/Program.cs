ee559d332657511ad4f147f65ac62f7bbc9a69ee
AlinCuc <alin.cucc@gmail.com>
2025-03-14 19:49:40 +0200

    added error handling, handling middleware, error responses

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrstructure.Data;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -17,9 +18,15 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
