3abcfc8e376280971ad641284fc28bad4c044b2e
AlinCuc <alin.cucc@gmail.com>
2025-05-13 11:39:50 +0300

    Added API-identity

--- Infrstructure/Data/StoreContext.cs
+++ Infrstructure/Data/StoreContext.cs
@@ -1,12 +1,14 @@
 ﻿using Core.Entities;
 using Infrstructure.Config;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrstructure.Data
 {
-    public class StoreContext(DbContextOptions options) : DbContext(options)
+    public class StoreContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<Address> Addresses { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
